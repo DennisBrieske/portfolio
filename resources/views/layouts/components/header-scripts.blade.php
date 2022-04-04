ca8ef1244226a5eaa2a15cb32c4a331459972268
dennisbrieske <brieske.dev@gmail.com>
2022-04-04 18:20:58 +0200

    Add Fontawesome 5.15.2 CDN

--- resources/views/layouts/components/header-scripts.blade.php
+++ resources/views/layouts/components/header-scripts.blade.php
@@ -2,9 +2,9 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <!-- Bootstrap core CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 <link href="{{asset('css/app.css')}}" rel="stylesheet">
+<link href="{{asset('css/style.css')}}" rel="stylesheet">
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}" defer></script>
