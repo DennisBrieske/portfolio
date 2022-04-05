665439a358a4248143e995415f3265c476f25115
dennisbrieske <brieske.dev@gmail.com>
2022-04-05 13:33:45 +0200

    Fix Text in Blog Cards to Lorem Ipsum

--- resources/views/layouts/components/blogCard.blade.php
+++ resources/views/layouts/components/blogCard.blade.php
@@ -5,10 +5,9 @@
     </div>
     <div class="card card-blog">
         <div class="card-body">
-            <h5 class="card-title fw-bold mt-2 mb-2 ms-3">Rover raised $65 million</h5>
+            <h5 class="card-title fw-bold mt-2 mb-2 ms-3">Laravel 9</h5>
             <p class="card-text mt-2 mb-2 ms-3 me-3">
-                Finding temporary housing for your dog should be as easy as
-                renting an Airbnb. That’s the idea behind Rover ...
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua...
             </p>
             <a href="#" class="btn btn-primary m-3 p-2" onclick="this.blur();">Read more</a>
         </div>
