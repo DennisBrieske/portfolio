fad98d6cb6bff12664e98333ae9721ac37392509
dennisbrieske <brieske.dev@gmail.com>
2022-04-05 13:55:28 +0200

    add time to read and card header icon

--- resources/views/layouts/components/blogCard.blade.php
+++ resources/views/layouts/components/blogCard.blade.php
@@ -1,15 +1,23 @@
-<div class="col-lg-3 mb-5 ms-4 me-4 boxShadow card-blogpost">
+<div class="col-lg-3 mb-5 me-5 boxShadow card-blogpost">
     <div class="c">
         <img src="{{asset('assets/images/testimonial/coding-1853305_1920.jpg')}}"
              alt="img" class="c-img" loading="lazy">
     </div>
     <div class="card card-blog">
         <div class="card-body">
-            <h5 class="card-title fw-bold mt-2 mb-2 ms-3">Laravel 9</h5>
+            <h5 class="card-title fw-bold mt-2 mb-2 ms-3"><i class="fas fa-code"></i> Laravel 9</h5>
             <p class="card-text mt-2 mb-2 ms-3 me-3">
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua...
+                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                labore et dolore magna aliquyam erat, sed diam voluptua ...
             </p>
-            <a href="#" class="btn btn-primary m-3 p-2" onclick="this.blur();">Read more</a>
+            <div class="row align-items-center card-title">
+                <div class="col-6">
+                    <a href="#" class="btn btn-primary m-3 p-2" onclick="this.blur();">Read more</a>
+                </div>
+                <div class="col-6 pe-3 text-end">
+                    <i class="far fa-clock"></i> 5 min read
+                </div>
+            </div>
         </div>
     </div>
 </div>
