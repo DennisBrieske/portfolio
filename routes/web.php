a052dc22bccf4a3d27e251ac5d490ad2ebaab54b
dennisbrieske <brieske.dev@gmail.com>
2022-04-08 12:35:59 +0200

    Set WelcomeController mehtod for simple user to "only index"

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\WelcomeController::class, 'index']);
+Route::resource('/', App\Http\Controllers\WelcomeController::class, ['only' => ['index']]);
 Route::resource('/blog', App\Http\Controllers\BlogController::class, ['only' => ['index', 'show']]);
 
 Auth::routes();
