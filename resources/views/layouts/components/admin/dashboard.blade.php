71a4c65d74dedb67ef2de688c3b0616a6085e210
dennisbrieske <brieske.dev@gmail.com>
2022-04-14 12:59:55 +0200

    Prepared Backend

---
+++ resources/views/layouts/components/admin/dashboard.blade.php
@@ -0,0 +1,18 @@
+<div class="m-5">
+    <div class="card blogOverview-card mt-3">
+        <div class="card-body">
+            <h5 class="card-title fw-bold mt-5 mb-2 ms-3"><i class="fas fa-code"></i> </h5>
+            <p class="blogOverview-card-text mt-5 mb-2 ms-3 me-3">
+
+            </p>
+            <div class="row align-items-center card-title">
+                <div class="col-6">
+                    <a href="{{ route('blog.index') }}" class="btn btn-primary m-3 p-2" onclick="this.blur();">Back to Overview</a>
+                </div>
+                <div class="col-6 pe-3 text-end">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/layouts/components/navbar.blade.php
+++ resources/views/layouts/components/navbar.blade.php
@@ -1,11 +1,11 @@
 <nav class="navigation p-5">
     <ul>
-        <li><span style="font-size: 150%"><a href="#"><i class="fab fa-twitter"></i></a></span></li>
-        <li class="me-3"><span style="font-size: 150%"><a href="#"><i class="fab fa-linkedin"></i></a></span></li>
-        <li class="me-3"><span style="font-size: 150%"><a href="#"><i class="fab fa-github"></i></a></span></li>
-        <li class="navigation-nav me-3 float-start"><span style="font-size: 150%"><a
+        <li class="me-3 navigation-nav-color"><span style="font-size: 150%"><a href="#"><i class="fab fa-twitter"></i></a></span></li>
+        <li class="me-3 navigation-nav-color"><span style="font-size: 150%"><a href="#"><i class="fab fa-linkedin"></i></a></span></li>
+        <li class="me-3 navigation-nav-color"><span style="font-size: 150%"><a href="#"><i class="fab fa-github"></i></a></span></li>
+        <li class="navigation-nav navigation-nav-color me-3 float-start"><span style="font-size: 150%"><a
                     href="{{ ('/') }}">Home</a></span></li>
-        <li class="navigation-nav me-3 float-start"><span style="font-size: 150%"><a href="{{ route('blog.index') }}">Blog</a></span>
+        <li class="navigation-nav navigation-nav-color me-3 float-start"><span style="font-size: 150%"><a href="{{ route('blog.index') }}">Blog</a></span>
         </li>
     </ul>
 </nav>
